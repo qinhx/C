4b16a51053ae81f284217704c0b864df38c87036
qinhx <835643409@qq.com>
2018-10-24 15:11:12 +0800

    dp algorithm

---
+++ DP/pack.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+class Solution{
+	public:
+};
+int main(){
+	int n;
+	Solution solu;
+	while(cin>>n){
+		
+	}
+}
---
+++ DP/virtural.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+class A{
+	public:
+		void foo(){
+			cout<<"1"<<endl;
+		}
+		virtual void fun(){
+			cout<<"2"<<endl;
+		}
+};
+class B:public A{
+	public:
+	void foo(){
+			cout<<"3"<<endl;
+		}
+	void fun(){
+			cout<<"4"<<endl;
+		}
+};
+int main(){
+	A a;
+	B b;
+	A *p = &a;
+	p->foo();
+	p->fun();
+	p=&b;
+	p->foo();
+	p->fun();
+	A *ptr = (A *)&b;
+	ptr->foo();
+	ptr->fun();
+	return 0;
+}
